79d01f4b466e4771619f287320cbb5a5c3801920
GonzaloPonce7 <gonzalo.p791@gmail.com>
2024-10-08 21:28:20 -0300

    Tabs nuevas para desarrollar1

--- src/app/apuestas/tab3.page.ts
+++ src/app/apuestas/tab3.page.ts
@@ -16,6 +16,27 @@ export class Tab3Page {
   constructor() {}
 
 
-  
+  // private geocodeDirecciones(): void {
+  //   this.documentos.forEach((documento) => {
+  //     const direccion = documento.direccion;
+  //     const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(direccion)}`;
+
+  //     fetch(url)
+  //       .then(response => response.json())
+  //       .then(data => {
+  //         if (data.length > 0) {
+  //           const latlng = new L.LatLng(data[0].lat, data[0].lon);
+  //           if (this.esCABA(latlng)) {
+  //             this.addMarker(latlng);
+  //           } else {
+  //             console.log('La dirección está fuera de CABA:', direccion);
+  //           }
+  //         }
+  //       })
+  //       .catch(error => {
+  //         console.error('Error al geocodificar la dirección:', error);
+  //       });
+  //   });
+  // }
 
 }
