1b40fd13e7b8fb58c6fd6d3028554756589e87f1
Mauro Galindez <maurogalindez.90@gmail.com>
2024-10-18 11:35:28 -0300

    fixed CORS problems by using a proxy that poses as a browser

---
+++ nodejsProxy/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:4200',
+}));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} request for '${req.url}'`);
+  next();
+});
+
+app.get('/proxy', async (req, res) => {
+  try {
+    const url = req.query.url;
+    console.log(`Fetching data from: ${url}`);
+    const response = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0'  // Mimic a browser request
+      }
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error(`Error fetching data: ${error}`);
+    res.status(500).send('Error fetching data');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Proxy server running on http://localhost:3000');
+});
