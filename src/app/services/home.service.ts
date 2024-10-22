d3fb533f4aa17da21b859acb1717c43616fa33e4
Alan <alanantezana1303@gmail.com>
2024-10-22 12:13:05 -0300

    Se configura proxy y api consume

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -6,27 +6,17 @@ import { lastValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class HomeService {
-  //API_KEY="4d72107548021849835a6010529e5c7c"; 
-  //BASE_URL = "http://localhost:8100/api/api.php";
-  //BASE_URL = "https://apiclient.besoccerapps.com/scripts/api/api.php";
-  //URL = `https://apiclient.besoccerapps.com/scripts/api/api.php?key=${this.API_KEY}&format=json&req=matchsday&date=${this.DATE}`;
   partidos: any;
 
   constructor(private httpClient: HttpClient) { }
 
   async getPartidos() {
-
     const headers = new HttpHeaders({
       'X-Auth-Token': 'feab5e72ee94431db81526ae1dd56d58'
     });
 
-    //const URL = `/api`;
-    //const URL = '/api/competitions/PL/matches?matchday=11';
-    //const URL = `/api?url=https://api.football-data.org/v4/competitions/PL/matches?matchday=8`;
-    const URL = `https://api.football-data.org/v4/competitions/PL/matches?matchday=8`;
-    //const URL = `/v4/competitions/PL/matches?matchday=8`;
-
-    //const URL = `${this.BASE_URL}?key=${params.key}&format=${params.format}&req=${params.req}&date=${params.date}`;
+    // Usa la URL del proxy
+    const URL = `http://localhost:3000/proxy?url=https://api.football-data.org/v4/competitions/PL/matches`;
 
     try {
       this.partidos = await lastValueFrom(this.httpClient.get(URL, { headers }));
